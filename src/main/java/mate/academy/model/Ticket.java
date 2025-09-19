c85b16bb777e3fc2affc6b15527ad33c739e473a
layzimayner <vasyahunta@gmail.com>
2025-09-19 19:11:24 +0300

    shopping cart

---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,56 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private MovieSession movie;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public MovieSession getMovie() {
+        return movie;
+    }
+
+    public void setMovie(MovieSession movie) {
+        this.movie = movie;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{"
+                + "id=" + id
+                + '}';
+    }
+}
